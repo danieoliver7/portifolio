9bdeb8e3a98381c5a00e382d2c52e0618196d4b5
Daniel Oliveira <danieoliver7@gmail.com>
2021-11-19 20:56:20 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portifólio Daniel</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+      <header class="masthead mb-auto">
+        <div class="inner">
+          <h3 class="masthead-brand">Daniel Oliveira</h3>
+          <nav class="nav nav-masthead justify-content-center">
+            <a class="nav-link" href="#">Trampos</a>
+            <a class="nav-link" href="#">Sobre</a>
+            <a class="nav-link" href="#">Contatos</a>
+          </nav>
+        </div>
+      </header>
+
+      <main role="main" class="inner cover">
+        <h1 class="cover-heading">Desenvolvedor </h1>
+        <p class="lead">Aqui eu considero como um link rápido para mostrar algumas coisas que já construí e considero como finalizadas, clica no link abaixo para acessar os trampos</p>
+        <p class="lead">
+          <a href="trampos.php" class="btn btn-lg btn-secondary">Trampos</a>
+        </p>
+      </main>
+
+      <footer class="mastfoot mt-auto">
+        
+      </footer>
+</div>
+    
+</body>
+</html>
